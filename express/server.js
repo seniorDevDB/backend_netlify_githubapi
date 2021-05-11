feefa0a335d2b026e8f518d970103cc826c39f9f
seniorDevDB <dbupworks@gmail.com>
2021-05-11 16:50:00 -0700

    updated

--- express/server.js
+++ express/server.js
@@ -6,11 +6,53 @@ const app = express();
 const bodyParser = require('body-parser');
 
 const router = express.Router();
+
+const axios = require('axios');
+
+const base_url = "https://jobs.github.com/positions.json"
+
 router.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write('<h1>Hello from Express.js!</h1>');
   res.end();
 });
+
+// Create User
+router.post('/getJobsPerPage', (req, res) => {
+  let url = base_url + '?page=' + req.body.pageNumber
+  console.log("url", url)
+  axios.get(url).then(result => {
+    res.send(JSON.stringify({
+      code: 'success',
+      data: result.data,
+      message: 'All Jobs'
+    }));
+  }).catch(err => {
+    res.send(JSON.stringify({
+      code: 'error',
+      message: 'Error'
+    }));
+  })
+});
+
+router.post('/getSearchJobs', (req, res) => {
+  const description = req.body.description;
+  const location = req.body.location;
+  const full_time = req.body.full_time;
+
+  let url = base_url + '?description=' + description + '&location=' + location + '&full_time=' + full_time;
+  console.log("url search", url)
+  axios.get(url).then(result => {
+    console.log("result", result)
+
+    res.send(JSON.stringify({
+      code: 'success',
+      data: result.data,
+      message: 'All Jobs'
+    }));
+  })
+})
+
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
