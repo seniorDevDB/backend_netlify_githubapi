4bc56de90bab3cb2f8bbdb77931e2ae7b7404131
seniorDevDB <dbupworks@gmail.com>
2021-05-11 17:05:30 -0700

    updated

--- express/server.js
+++ express/server.js
@@ -5,6 +5,8 @@ const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
 
+const cors = require('cors');
+
 const router = express.Router();
 
 const axios = require('axios');
@@ -57,6 +59,7 @@ router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
+app.use(cors());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
